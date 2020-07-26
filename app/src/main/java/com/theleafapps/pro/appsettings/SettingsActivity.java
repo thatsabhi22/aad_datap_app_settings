e54892c4556ba748f45465c75c82b32c768b89e0
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-07-26 12:05:19 +0530

    Added SettingsActivity and PrefrenceFragment to the same activity

--- app/src/main/java/com/theleafapps/pro/appsettings/SettingsActivity.java
+++ app/src/main/java/com/theleafapps/pro/appsettings/SettingsActivity.java
@@ -9,6 +9,9 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
 }
